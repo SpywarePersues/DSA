ab2b461bfdb1b3c98c41aa475310caa12924aaf0
SpywarePersues <spywarepersues284@gmail.com>
2022-10-24 16:51:19 +0530

    4.1.1 + 4.1.2 + 4.1.3

---
+++ 4.1.1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int row, col;
+
+    cout << "Enter the number of rows: ";
+    cin >> row;
+
+    cout << "Enter the number of columns: ";
+    cin >> col;
+
+    for (int i = 1; i <= row; i++)
+    {
+        for (int j = 1; j <= col; j++)
+        {
+            cout << "✨";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
