8e5647946f61e3e04d132849ba115f5f1e23895f
SpywarePersues <spywarepersues284@gmail.com>
2022-10-24 15:35:33 +0530

    3.2.2

--- 3.2.3.cpp
+++ 3.2.3.cpp
@@ -4,11 +4,13 @@ using namespace std;
 int main()
 {
     float n1, n2;
-    cout << "Write 2 numbers: ";
-    cin >> n1 >> n2;
+    cout << "Write First number: ";
+    cin >> n1;
+    cout << "Write Second number: ";
+    cin >> n2;
 
     char op;
-    cout << "Input an operator: ";
+    cout << "Write an operator: ";
     cin >> op;
 
     switch (op)
